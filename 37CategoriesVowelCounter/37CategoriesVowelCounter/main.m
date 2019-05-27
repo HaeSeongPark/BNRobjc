55c246f81726f072bef6f7a7ce759ad4315d8da6
rhino <cord7894@gmail.com>
2019-05-27 21:56:17 +0900

    complete ch38 bitwise operations

--- 37CategoriesVowelCounter/37CategoriesVowelCounter/main.m
+++ 37CategoriesVowelCounter/37CategoriesVowelCounter/main.m
@@ -4,7 +4,7 @@
 //
 //  Created by rhino Q on 25/05/2019.
 //  Copyright © 2019 rhino Q. All rights reserved.
-//
+// https://forums.bignerdranch.com/c/objective-c-programming-2nd-edition/38-bitwise-operations
 
 #import <Foundation/Foundation.h>
 #import "NSString+BNRVowelCounting.h"
@@ -12,12 +12,30 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSString *string = @"Hello World!";
-        NSLog(@"%@ has %d vowels", string, [string bnr_vowelCount]);
+//        NSString *string = @"Hello World!";
+//        NSLog(@"%@ has %d vowels", string, [string bnr_vowelCount]);
+//
+//        NSDate *date = [NSDate new];
+//        [date bnr_makeDateWithYear:2019 month:10 day:10];
+//        NSLog(@"test : %@", date);
         
-        NSDate *date = [NSDate new];
-        [date bnr_makeDateWithYear:2019 month:10 day:10];
-        NSLog(@"test : %@", date);
+        uint64_t num = 1llu;
+        
+//        for(int i=1; i<=64; i++)
+//        {
+//            num = num << 1;
+//            if(i%2 == 0){
+//                num += 1;
+//            }
+//        }
+        
+        for(int i=0; i<64; i+=2)
+        {
+            num |= (1llu<<i);
+        }
+        
+        
+        NSLog(@"The number is %llu", num);
     }
     return 0;
 }
